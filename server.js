f0cafb8c64ba6366a1aaf8cf818db7821f47ad8a
Eric Williams <eric@churchwilliams.com>
2015-02-13 15:45:53 -0700

    did a bunch of this. Very much not done though

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express');
-
 var app = express();
+var Mongoose = require('mongoose');
+var MongoUri = "mongodb://localhost:27017/song-tag";
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+//Controllers
+
+var ArtistCtrl = require('./lib/controllers/artistCtrl.js');
+
+Mongoose.connect(MongoUri);
+Mongoose.connection.once("open", function(){
+	console.log("Connected to db " + MongoUri);
+});
+
 
 //routes go here
 
 app.listen(8000);
+
+app.post("/artist", ArtistCtrl.post);
